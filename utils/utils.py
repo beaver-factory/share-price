929e15e1fe4e4a7c3cfe76b2b1fcb190f715c722
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-08-23 15:18:34 +0100

    feat: merge func and insert func

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,28 @@ def calculate_price_change(current_price, last_price, ):
     return (diff/last_price)*100
 
 
-def insert_into_html(base_path, new_path):
+def insert_into_html(base_path, copy):
+    from bs4 import BeautifulSoup
+
+    with open(base_path, 'r', encoding='utf-8') as html_file:
+        base_content = html_file.read()
+
+    soup = BeautifulSoup(base_content, 'html.parser')
+
+    found_element = soup.find(id='stock_data_2023')
+
+    if found_element:
+        new_tag = soup.new_tag("div")
+        new_tag.append(BeautifulSoup(copy, 'html.parser'))
+        found_element.append(new_tag)
+        amended_html = soup.prettify()
+
+        return amended_html
+    else:
+        print("Element not found.")
+
+
+def merge_html(base_path, new_path):
     from bs4 import BeautifulSoup
 
     with open(base_path, 'r', encoding='utf-8') as html_file:
