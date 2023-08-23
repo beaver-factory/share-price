ac4d357d1c1997e8cdc18191ccd5a506d228cefe
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-08-23 16:13:25 +0100

    feat: add extra share data info

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from send_share_email import send_share_email
 from get_share_price import get_latest_closing_share_price
+from utils.utils import calculate_price_change
 
 
 def main():
@@ -9,8 +10,16 @@ def main():
 
     share_price = get_latest_closing_share_price()
 
+    week_change = calculate_price_change(share_price.day, share_price.week)
+    month_change = calculate_price_change(share_price.day, share_price.month)
+    half_change = calculate_price_change(
+        share_price.day, share_price.half_year)
+    year_change = calculate_price_change(share_price.day, share_price.year)
+
+    html_data_string = f'<div><h2>Your Northcoders share price update for today!</h2><h3>The current share price is: {share_price.day}</h3><table><tr><th>Time Period</th><th>Price</th><th>% Change</th></tr><tr><td>Today</td><td>{share_price.day}</td><td>0</td></tr><tr><td>Last Week</td><td>{share_price.week}</td><td>{week_change}%</td></tr><tr><td>Last 28 Days</td><td>{share_price.month}</td><td>{month_change}%</td></tr><tr><td>Last 6 Months</td><td>{share_price.half_year}</td><td>{half_change}</td></tr><tr><td>Last Year</td><td>{share_price.year}</td><td>{year_change}</td></tr></table></div>'
+
     for user in users:
-        send_share_email(user, share_price)
+        send_share_email(user, html_data_string)
 
 
 main()
--- send_share_email.py
+++ send_share_email.py
@@ -7,10 +7,10 @@ from .utils.utils import insert_into_html
 load_dotenv()
 
 
-def send_share_email(user_email, share_price):
+def send_share_email(user_email, html_data_string):
     SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
-    email_body = insert_into_html('email_copy.html', share_price)
+    email_body = insert_into_html('email_copy.html', html_data_string)
 
     message = Mail(
         from_email='teyah.brennen-davies@northcoders.com',
