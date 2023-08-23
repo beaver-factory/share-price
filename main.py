a3061bb4c664d724dccece47366b9bafcb624cd3
xav <73826012+gravybrux@users.noreply.github.com>
2023-08-23 14:58:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from get_share_price import get_latest_closing_share_price
 
 def main():
     latest_share_price = get_latest_closing_share_price()
+    print(latest_share_price)
     return latest_share_price
