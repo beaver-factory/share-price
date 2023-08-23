81b76ea3caec4c205c8cbe51faaf85403becb787
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-08-23 15:28:05 +0100

    feat: email body with nc stuff

--- send_share_email.py
+++ send_share_email.py
@@ -2,6 +2,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from dotenv import load_dotenv
+from .utils.utils import insert_into_html
 
 load_dotenv()
 
@@ -9,11 +10,14 @@ load_dotenv()
 def send_share_email(user_email, share_price):
     SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
+    email_body = insert_into_html('email_copy.html', share_price)
+
     message = Mail(
         from_email='teyah.brennen-davies@northcoders.com',
         to_emails=user_email,
         subject='Your NC Share Price Update!',
-        html_content=f'<strong>This is a share price: {share_price}</strong>')
+        html_content=email_body
+    )
 
     sg = SendGridAPIClient(SENDGRID_API_KEY)
 
