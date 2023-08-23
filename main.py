cc462aeedea510f46ceb950e529ab5425dd9de50
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-08-23 15:06:37 +0100

    refactor: extract email sending to separate function

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
 import json
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from dotenv import load_dotenv
-from get_share_price import get_latest_closing_share_price
-
-
-load_dotenv()
+from send_share_email import send_share_email
 
 
 def main():
@@ -15,21 +8,3 @@ def main():
 
     for user in users:
         send_share_email(user)
-
-
-def send_share_email(user_email):
-    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-
-    share_price = get_latest_closing_share_price()
-
-    message = Mail(
-        from_email='teyah.brennen-davies@northcoders.com',
-        to_emails=user_email,
-        subject='Your NC Share Price Update',
-        html_content=f'<strong>This is a share price: {share_price}</strong>')
-
-    sg = SendGridAPIClient(SENDGRID_API_KEY)
-
-    response = sg.send(message)
-
-    print(response.status_code, response.body, response.headers)
