707cf9af92b9a7abde0936986181404d6c2eb4d1
Alex Cupit <alexcupit@outlook.com>
2023-08-23 14:44:01 +0100

    feat: add get_latest_closing_share_price

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello world')
+from get_share_price import get_latest_closing_share_price
+
+
+def main():
+    latest_share_price = get_latest_closing_share_price()
+    return latest_share_price
