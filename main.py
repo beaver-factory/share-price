d3adc7297214ffeb9692baf16fd7f932e621eb0e
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-09-13 16:15:42 +0100

    fix: rounding share price \

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     with open('users.json', 'r') as f:
         users = json.load(f)
 
-    share_price = round(get_share_prices(), 2)
+    share_price = get_share_prices()
 
     week_change = round(calculate_price_change(
         share_price["day"], share_price["week"]), 2)
@@ -31,7 +31,7 @@ def main():
         share_price["day"], share_price["year"]), 2)
 
     html_data_string = f"""
-    <h3>The current share price is: £{share_price["day"]}</h3>
+    <h3>The current share price is: £{round(share_price["day"], 2)}</h3>
     <table border='1' style='border-collapse:collapse;'>
     <tr>
         <th style="padding: 10px">Time Period</th>
@@ -41,14 +41,14 @@ def main():
     </tr>
     <tr>
         <td style="padding: 10px;">Today</td>
-        <td style="padding: 10px;">£{share_price["day"]}</td>
+        <td style="padding: 10px;">£{round(share_price["day"],2)}</td>
         <td style="padding: 10px;">-</td>
         <td style="padding: 10px;">-</td>
     </tr>
-    {generate_row('Last Week', share_price['week'], week_change, week_change_abs)}
-    {generate_row('Last 28 Days', share_price['month'], month_change, month_change_abs)}
-    {generate_row('Last 6 Months', share_price['half_year'], half_change, half_change_abs)}
-    {generate_row('Last Year', share_price['year'], week_change, week_change_abs)}
+    {generate_row('Last Week', round(share_price['week'],2), week_change, week_change_abs)}
+    {generate_row('Last 28 Days', round(share_price['month'],2), month_change, month_change_abs)}
+    {generate_row('Last 6 Months', round(share_price['half_year'],2), half_change, half_change_abs)}
+    {generate_row('Last Year', round(share_price['year'],2), week_change, week_change_abs)}
     </table>
     """
 
