28b5e674da78e28d808079e5351f85e1b5641758
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-09-13 16:35:51 +0100

    fix: divide stonk price by 100 :(

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def main():
     week_change = round(calculate_price_change(
         share_price["day"], share_price["week"]), 2)
     week_change_abs = round(calculate_absolute_change(
-        share_price["day"], share_price["week"]), 2)
+        share_price["day"], share_price["week"])/100, 2)
 
     month_change = round(calculate_price_change(
         share_price["day"], share_price["month"]), 2)
     month_change_abs = round(calculate_absolute_change(
-        share_price["day"], share_price["month"]), 2)
+        share_price["day"], share_price["month"])/100, 2)
 
     half_change = round(calculate_price_change(
         share_price["day"], share_price["half_year"]), 2)
     half_change_abs = round(calculate_absolute_change(
-        share_price["day"], share_price["half_year"]), 2)
+        share_price["day"], share_price["half_year"])/100, 2)
 
     year_change = round(calculate_price_change(
         share_price["day"], share_price["year"]), 2)
     year_change_abs = round(calculate_absolute_change(
-        share_price["day"], share_price["year"]), 2)
+        share_price["day"], share_price["year"])/100, 2)
 
     html_data_string = f"""
     <h3>The current share price is: £{round(share_price["day"], 2)}</h3>
@@ -45,10 +45,10 @@ def main():
         <td style="padding: 10px;">-</td>
         <td style="padding: 10px;">-</td>
     </tr>
-    {generate_row('Last Week', round(share_price['week'],2), week_change, week_change_abs)}
-    {generate_row('Last 28 Days', round(share_price['month'],2), month_change, month_change_abs)}
-    {generate_row('Last 6 Months', round(share_price['half_year'],2), half_change, half_change_abs)}
-    {generate_row('Last Year', round(share_price['year'],2),year_change, year_change_abs)}
+    {generate_row('Last Week', round(share_price['week']/100,2), week_change, week_change_abs)}
+    {generate_row('Last 28 Days', round(share_price['month']/100,2), month_change, month_change_abs)}
+    {generate_row('Last 6 Months', round(share_price['half_year']/100,2), half_change, half_change_abs)}
+    {generate_row('Last Year', round(share_price['year']/100,2),year_change, year_change_abs)}
     </table>
     """
 
