c4940561159c9f0bfd696849fb6e9ba6d7321803
Chris-Duignan <cduignan.1993@gmail.com>
2023-09-13 16:49:55 +0100

    feat: add basic post to slack

--- post_to_slack.py
+++ post_to_slack.py
@@ -1,6 +1,11 @@
 import requests
+import os
+import json
 from tabulate import tabulate
 from get_share_price import get_share_prices
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def post_message_to_slack(share_price, changes):
@@ -11,9 +16,13 @@ def post_message_to_slack(share_price, changes):
     
     {tabulate([
         ['Today', share_price["day"], 0, "£0"],
-        ['Last Week', share_price['week'], changes['week_change'], f"£{changes['week_change_abs']}"],
-        ['Last 28 Days', share_price['month'], changes['month_change'], f"£{changes['month_change_abs']}"],
-        ['Last 6 months', share_price['half_year'], changes['half_change'], f"£{changes['half_change_abs']}"],
-        ['Last Year', share_price['year'], changes['year_change'], f"£{changes['year_change_abs']}"]
-    ], headers=['Time Period', 'Share Price', '% Change', '£ Change'])}
+        ['Last Week', share_price['week'], changes['week_change'], f"{changes['week_change_abs']}p"],
+        ['Last 28 Days', share_price['month'], changes['month_change'], f"£{changes['month_change_abs']}p"],
+        ['Last 6 months', share_price['half_year'], changes['half_change'], f"£{changes['half_change_abs']}p"],
+        ['Last Year', share_price['year'], changes['year_change'], f"£{changes['year_change_abs']}p"]
+    ], headers=['Time Period', 'Share Price', '% Change', 'p Change'])}
     """
+
+    return requests.post(os.getenv('WEBHOOK_URL'), json.dumps({
+        'text': copy
+    }))
