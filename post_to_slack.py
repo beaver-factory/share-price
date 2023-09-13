b6809a252a159d3e94634e05e46ee65985cff23e
Chris-Duignan <cduignan.1993@gmail.com>
2023-09-13 17:16:43 +0100

    fix: change unit for £change in slack post

--- post_to_slack.py
+++ post_to_slack.py
@@ -16,10 +16,10 @@ def post_message_to_slack(share_price, changes):
     
     {tabulate([
         ['Today', share_price["day"], 0, "£0"],
-        ['Last Week', share_price['week'], changes['week_change'], f"{changes['week_change_abs']}p"],
-        ['Last 28 Days', share_price['month'], changes['month_change'], f"£{changes['month_change_abs']}p"],
-        ['Last 6 months', share_price['half_year'], changes['half_change'], f"£{changes['half_change_abs']}p"],
-        ['Last Year', share_price['year'], changes['year_change'], f"£{changes['year_change_abs']}p"]
-    ], headers=['Time Period', 'Share Price', '% Change', 'p Change'])}
+        ['Last Week', share_price['week'], changes['week_change'], f"{changes['week_change_abs']}"],
+        ['Last 28 Days', share_price['month'], changes['month_change'], f"£{changes['month_change_abs']}"],
+        ['Last 6 months', share_price['half_year'], changes['half_change'], f"£{changes['half_change_abs']}"],
+        ['Last Year', share_price['year'], changes['year_change'], f"£{changes['year_change_abs']}"]
+    ], headers=['Time Period', 'Share Price', '% Change', '£ Change'])}
     """
     return requests.post(os.getenv("WEBHOOK_URL"), json.dumps({"channel": "nc-shares", "text": copy}))
